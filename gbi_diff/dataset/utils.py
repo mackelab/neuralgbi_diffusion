608078d3df6b762d318c366d83a3e19a1db36316
Robin Uhrich <robin.uhrich@gmail.com>
2024-11-06 17:17:16 +0100

    renamed variables

--- gbi_diff/dataset/utils.py
+++ gbi_diff/dataset/utils.py
@@ -4,5 +4,5 @@ from sourcerer.simulators import TwoMoonsSimulator
 def generate_moon(size: int, mean_radius: float = 0.1, std_radius: float = 0.01):
     simulator = TwoMoonsSimulator(mean_radius=mean_radius, std_radius=std_radius)
     prior = simulator.sample_prior(size)
-    posterior = simulator.sample(prior)
-    return prior, posterior
+    likelihood_samples = simulator.sample(prior)
+    return prior, likelihood_samples
