850bc4ae2c9cdf9962196e2cdf68c6336416b8fc
Robin Uhrich <robin.uhrich@gmail.com>
2024-11-12 13:08:56 +0100

    formatting

--- gbi_diff/dataset/utils.py
+++ gbi_diff/dataset/utils.py
@@ -1,8 +1,9 @@
 from sourcerer.simulators import TwoMoonsSimulator
 
 
-def generate_moon(size: int, mean_radius: float = 0.1, std_radius: float = 0.01):
-    simulator = TwoMoonsSimulator(mean_radius=mean_radius, std_radius=std_radius)
+# use standard parameters
+def generate_moon(size: int):
+    simulator = TwoMoonsSimulator()
     prior = simulator.sample_prior(size)
     likelihood_samples = simulator.sample(prior)
     return prior, likelihood_samples
