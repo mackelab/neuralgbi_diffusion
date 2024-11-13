d8c0a479498d0155474755c675fae42a66653d6b
Robin Uhrich <robin.uhrich@gmail.com>
2024-11-13 13:35:56 +0100

    create also directory structure while writing yaml

--- gbi_diff/utils/filesystem.py
+++ gbi_diff/utils/filesystem.py
@@ -1,7 +1,13 @@
+import os
 from typing import Any, Dict
 import yaml
 
+
 def write_yaml(content: Dict[str, Any], path: str): 
+    directory = "/".join(path.split("/")[:-1])
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     with open(path, "w", encoding="utf-8") as file:
         yaml.dump(content, file)
         
