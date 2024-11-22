9fa12318bdd650f59c814ce97871da22f4d49ddf
Robin Uhrich <robin.uhrich@gmail.com>
2024-11-22 10:25:12 +0100

    added sir simulator

--- gbi_diff/dataset/simulator_api.py
+++ gbi_diff/dataset/simulator_api.py
@@ -2,11 +2,14 @@ from sourcerer.simulators import (
     TwoMoonsSimulator,
     LotkaVolterraSimulator,
     InverseKinematicsSimulator,
+    SIRSimulator,
 )
+
+# TODO: generalize this API function
 
 
 # use standard parameters
-def generate_two_moon(size: int):
+def generate_two_moons(size: int):
     simulator = TwoMoonsSimulator()
     prior = simulator.sample_prior(size)
     likelihood_samples = simulator.sample(prior)
@@ -22,6 +25,13 @@ def generate_lotka_volterra(size: int):
 
 def generate_inverse_kinematics(size: int):
     simulator = InverseKinematicsSimulator()
+    prior = simulator.sample_prior(size)
+    likelihood_samples = simulator.sample(prior)
+    return prior, likelihood_samples
+
+
+def generate_sir(size: int):
+    simulator = SIRSimulator()
     prior = simulator.sample_prior(size)
     likelihood_samples = simulator.sample(prior)
     return prior, likelihood_samples
