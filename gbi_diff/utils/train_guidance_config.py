4592190805e5c5976a242c46cfdba41db5ca0edf
Robin Uhrich <robin.uhrich@gmail.com>
2025-01-08 12:45:03 +0100

    updated configs along with their classes

--- gbi_diff/utils/train_guidance_config.py
+++ gbi_diff/utils/train_guidance_config.py
@@ -22,6 +22,32 @@ class _Model(StructuredConfig):
 
 
 @dataclass
+class _UniformSampler(StructuredConfig):
+    n_samples: int
+
+
+@dataclass
+class _VPSchedule(StructuredConfig):
+    beta_start: float
+    beta_end: float
+    beta_schedule_cls: str
+
+
+@dataclass
+class _Diffusion(StructuredConfig):
+    steps: int
+    include_t: bool
+    diffusion_time_sampler: str
+    diffusion_schedule: str
+    UniformSampler: _UniformSampler
+    VPSchedule: _VPSchedule
+
+    def __post_init__(self):
+        self.UniformSampler = _UniformSampler(**self.UniformSampler)  #pylint: disable=E1134
+        self.VPSchedule = _VPSchedule(**self.VPSchedule)  #pylint: disable=E1134
+
+
+@dataclass
 class _Optimizer(StructuredConfig):
     name: str
     lr: float
@@ -38,9 +64,11 @@ class Config(StructuredConfig):
     precision: int
     dataset: _Dataset
     model: _Model
+    diffusion: _Diffusion
     optimizer: _Optimizer
 
     def __post_init__(self):
         self.dataset = _Dataset(**self.dataset)  #pylint: disable=E1134
         self.model = _Model(**self.model)  #pylint: disable=E1134
+        self.diffusion = _Diffusion(**self.diffusion)  #pylint: disable=E1134
         self.optimizer = _Optimizer(**self.optimizer)  #pylint: disable=E1134
