dc7df3d42e98e631090582941fe78e2b12aec1df
Robin Uhrich <robin.uhrich@gmail.com>
2025-01-10 16:17:40 +0100

    updated config to class deamon

--- start_c2c_services.sh
+++ start_c2c_services.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+config2class stop-all
 config2class start-service --input config/train.yaml --output gbi_diff/utils/train_config.py
 config2class start-service --input config/train_guidance.yaml --output gbi_diff/utils/train_guidance_config.py
 config2class start-service --input config/train_diffusion.yaml --output gbi_diff/utils/train_diffusion_config.py
