43d95f982442b5452c1c4ac7a4173e1d05eb0069
Robin Uhrich <robin.uhrich@gmail.com>
2025-01-10 14:13:50 +0100

    updated config

--- start_c2c_services.sh
+++ start_c2c_services.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 config2class start-service --input config/train.yaml --output gbi_diff/utils/train_config.py
-config2class start-service --input config/train_theta_noise.yaml --output gbi_diff/utils/train_theta_noise_config.py
+config2class start-service --input config/train_guidance.yaml --output gbi_diff/utils/train_guidance_config.py
+config2class start-service --input config/train_diffusion.yaml --output gbi_diff/utils/train_diffusion_config.py
 config2class start-service --input config/mcmc.yaml --output gbi_diff/utils/mcmc_config.py
