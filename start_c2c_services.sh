4bf355451b94cc42fc5e2f39bf360774af6a60ae
Robin Uhrich <robin.uhrich@gmail.com>
2025-01-14 13:29:44 +0100

    updated config2class start script

--- start_c2c_services.sh
+++ start_c2c_services.sh
@@ -4,4 +4,5 @@ config2class stop-all
 config2class start-service --input config/train.yaml --output gbi_diff/utils/train_config.py
 config2class start-service --input config/train_guidance.yaml --output gbi_diff/utils/train_guidance_config.py
 config2class start-service --input config/train_diffusion.yaml --output gbi_diff/utils/train_diffusion_config.py
-config2class start-service --input config/mcmc.yaml --output gbi_diff/utils/mcmc_config.py
+config2class start-service --input config/sampling_mcmc.yaml --output gbi_diff/utils/sampling_mcmc_config.py
+config2class start-service --input config/sampling_diffusion.yaml --output gbi_diff/utils/sampling_diffusion_config.py
