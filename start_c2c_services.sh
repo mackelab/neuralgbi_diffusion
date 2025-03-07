61be7ae45bb36e7495282ca1618fe34c93b9d543
Robin Uhrich <robin.uhrich@gmail.com>
2025-03-07 08:53:47 +0100

    updated c2c services

--- start_c2c_services.sh
+++ start_c2c_services.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 config2class stop-all
-config2class start-service --input config/train.yaml --output gbi_diff/utils/train_config.py
+config2class start-service --input config/train_potential.yaml --output gbi_diff/utils/train_potential_config.py
 config2class start-service --input config/train_guidance.yaml --output gbi_diff/utils/train_guidance_config.py
 config2class start-service --input config/train_diffusion.yaml --output gbi_diff/utils/train_diffusion_config.py
 config2class start-service --input config/sampling_mcmc.yaml --output gbi_diff/utils/sampling_mcmc_config.py
 config2class start-service --input config/sampling_diffusion.yaml --output gbi_diff/utils/sampling_diffusion_config.py
+config2class start-service --input config/evaluate_diffusion.yaml --output gbi_diff/utils/evaluate_diffusion_config.py
