50f45da11161957a3784acaa2288956e97a70cdd
Robin Uhrich <robin.uhrich@gmail.com>
2025-03-26 12:49:02 +0100

    moved train utils into separate file

--- gbi_diff/scripts/train.py
+++ gbi_diff/scripts/train.py
@@ -1,105 +1,15 @@
-import logging
-import sys
-
-import torch
-import yaml
-from lightning import Trainer
-from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
-from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
-from torch.utils.data import DataLoader
-
-from gbi_diff.dataset import dataset as sbi_datasets
-from gbi_diff.model.lit_module import DiffusionModel, PotentialNetwork, Guidance
-from gbi_diff.utils.cast import to_camel_case
-from gbi_diff.utils.train_config import Config as Config_Potential
-from gbi_diff.utils.train_guidance_config import Config as Config_Guidance
-from gbi_diff.utils.train_diffusion_config import Config as Config_Diffusion
-
-
-def _setup_trainer(
-    config,
-    devices: int = 1,
-) -> Trainer:
-    # TODO: fixup device config
-    accelerator = "auto"
-    if not torch.cuda.is_available() and devices > 1:
-        logging.warning("cuda device was requested but not available. Fall back to cpu")
-        devices = 1
-        accelerator = "cpu"
-
-    # setup logger
-    tb_logger = TensorBoardLogger(config.results_dir, log_graph=True)
-    csv_logger = CSVLogger(tb_logger.log_dir, name="csv_logs", version="")
-
-    trainer = Trainer(
-        default_root_dir=config.results_dir,
-        logger=(
-            # NOTE: make sure tensor board stays at first places
-            tb_logger,
-            csv_logger,
-        ),
-        precision=config.precision,
-        max_epochs=config.max_epochs,
-        check_val_every_n_epoch=config.check_val_every_n_epochs,
-        callbacks=[
-            ModelCheckpoint(
-                dirpath=tb_logger.log_dir,
-                monitor="val/loss",
-                save_top_k=3,
-                mode="min",
-            ),
-            LearningRateMonitor("epoch"),
-        ],
-        accelerator=accelerator,
-        devices=devices,
-        log_every_n_steps=10
-    )
-
-    log_dir = tb_logger.log_dir
-    return trainer, log_dir
-
-
-def _setup_datasets(config):
-    cls_name = to_camel_case(config.data_entity)
-    cls_name = cls_name[0].upper() + cls_name[1:]
-    dataset_cls = getattr(sbi_datasets, cls_name)
-    train_set: sbi_datasets._SBIDataset = dataset_cls.from_file(
-        config.dataset.train_file
-    )
-
-    train_set.set_n_target(config.dataset.n_target)
-    train_loader = DataLoader(
-        train_set,
-        batch_size=config.batch_size,
-        shuffle=True,
-        num_workers=config.num_worker,
-    )
-    val_set: sbi_datasets._SBIDataset = dataset_cls.from_file(config.dataset.val_file)
-    val_set.set_n_target(config.dataset.n_target)
-    val_loader = DataLoader(
-        val_set,
-        batch_size=config.batch_size,
-        shuffle=False,
-        num_workers=config.num_worker,
-    )
-    return train_loader, train_set, val_loader, val_set
-
-
-def _print_state(config, model):
-    print("============= Config ===============")
-    print(yaml.dump(config.to_container(), indent=4))
-    print("============== Net =================")
-    print(model)
-
-
-def _ask(force: bool):
-    if force:
-        return
-
-    question = input("Would you like to start to train? [Y, n]")
-    if not (question is None or question.lower().strip() in ["", "y", "yes"]):
-        print("Abort training")
-        sys.exit()
+from gbi_diff.dataset.dataset import GaussianMixture
+from gbi_diff.model.lit_module import DiffusionModel, Guidance, PotentialNetwork
+from gbi_diff.utils.configs.train_diffusion import Config as Config_Diffusion
+from gbi_diff.utils.configs.train_guidance import Config as Config_Guidance
+from gbi_diff.utils.configs.train_potential import Config as Config_Potential
+from gbi_diff.utils.metrics import mmd_dist, mse_dist
+from gbi_diff.utils.train_utils import (
+    _ask,
+    _print_state,
+    _setup_datasets,
+    _setup_trainer,
+)
 
 
 def train_potential(config: Config_Potential, devices: int = 1, force: bool = False):
@@ -109,12 +19,21 @@ def train_potential(config: Config_Potential, devices: int = 1, force: bool = Fa
     trainer, log_dir = _setup_trainer(config, devices)
     train_loader, train_set, val_loader, _ = _setup_datasets(config)
 
+    if isinstance(train_set, GaussianMixture):
+        trial_dim = train_set._x.shape[1]
+        dist_func = mmd_dist
+    else:
+        trial_dim = 0
+        dist_func = mse_dist
+
     model = PotentialNetwork(
         theta_dim=train_set.get_theta_dim(),
         simulator_out_dim=train_set.get_sim_out_dim(),
         optimizer_config=config.optimizer,
         net_config=config.model,
+        trial_dim=trial_dim,
     )
+    model.init_wrt_dataset(train_set._theta, train_set._x, train_set._x_miss, dist_func)
 
     _print_state(config, model)
     _ask(force)
@@ -129,13 +48,23 @@ def train_guidance(config: Config_Guidance, devices: int = 1, force: bool = Fals
     trainer, log_dir = _setup_trainer(config, devices)
     train_loader, train_set, val_loader, _ = _setup_datasets(config)
 
+    if isinstance(train_set, GaussianMixture):
+        trial_dim = train_set._x.shape[1]
+        dist_func = mmd_dist
+    else:
+        trial_dim = 0
+        dist_func = mse_dist
+
     model = Guidance(
         theta_dim=train_set.get_theta_dim(),
         simulator_out_dim=train_set.get_sim_out_dim(),
         optimizer_config=config.optimizer,
         net_config=config.model,
         diff_config=config.diffusion,
+        trial_dim=trial_dim,
     )
+    model.init_wrt_dataset(train_set._theta, train_set._x, train_set._x_miss, dist_func)
+    
     _print_state(config, model)
     _ask(force)
 
---
+++ gbi_diff/utils/train_utils.py
@@ -0,0 +1,104 @@
+import logging
+import sys
+from typing import Tuple
+
+import torch
+import yaml
+from lightning import Trainer
+from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
+from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
+from torch import nn
+from torch.utils.data import DataLoader
+
+from gbi_diff.dataset import dataset as sbi_datasets
+from gbi_diff.utils.cast import to_camel_case
+
+
+def _setup_trainer(
+    config,
+    devices: int = 1,
+) -> Trainer:
+    # TODO: fixup device config
+    accelerator = "auto"
+    if not torch.cuda.is_available() and devices > 1:
+        logging.warning("cuda device was requested but not available. Fall back to cpu")
+        devices = 1
+        accelerator = "cpu"
+
+    # setup logger
+    tb_logger = TensorBoardLogger(config.results_dir, log_graph=True)
+    csv_logger = CSVLogger(tb_logger.log_dir, name="csv_logs", version="")
+
+    trainer = Trainer(
+        default_root_dir=config.results_dir,
+        logger=(
+            # NOTE: make sure tensor board stays at first places
+            tb_logger,
+            csv_logger,
+        ),
+        precision=config.precision,
+        max_epochs=config.max_epochs,
+        check_val_every_n_epoch=config.check_val_every_n_epochs,
+        callbacks=[
+            ModelCheckpoint(
+                dirpath=tb_logger.log_dir,
+                monitor="val/loss",
+                save_top_k=3,
+                mode="min",
+            ),
+            LearningRateMonitor("epoch"),
+        ],
+        accelerator=accelerator,
+        devices=devices,
+        log_every_n_steps=10,
+    )
+
+    log_dir = tb_logger.log_dir
+    return trainer, log_dir
+
+
+def _setup_datasets(
+    config,
+) -> Tuple[DataLoader, DataLoader, DataLoader, sbi_datasets._SBIDataset]:
+    cls_name = to_camel_case(config.data_entity)
+    cls_name = cls_name[0].upper() + cls_name[1:]
+    dataset_cls = getattr(sbi_datasets, cls_name)
+    train_set: sbi_datasets._SBIDataset = dataset_cls.from_file(
+        config.dataset.train_file
+    )
+
+    train_set.set_n_target(config.dataset.n_target)
+    train_loader = DataLoader(
+        train_set,
+        batch_size=config.batch_size,
+        shuffle=True,
+        num_workers=config.num_worker,
+    )
+    val_set: sbi_datasets._SBIDataset = dataset_cls.from_file(config.dataset.val_file)
+    val_set.set_n_target(config.dataset.n_target)
+    val_loader = DataLoader(
+        val_set,
+        batch_size=config.batch_size,
+        shuffle=False,
+        num_workers=config.num_worker,
+    )
+    return train_loader, train_set, val_loader, train_set
+
+
+def _print_state(config, model: nn.Module):
+    print("============= Config ===============")
+    print(yaml.dump(config.to_container(), indent=4))
+    print("============== Net =================")
+    print(model)
+    n_params = sum([len(param.flatten()) for param in model.parameters()])
+    print("Number of parameters: ", n_params)
+
+
+def _ask(force: bool):
+    if force:
+        return
+
+    question = input("Would you like to start to train? [Y, n]")
+    if not (question is None or question.lower().strip() in ["", "y", "yes"]):
+        print("Abort training")
+        sys.exit()
